8b8e404a167c315ac5fa4943f9af7413c6fb1205
fernandes99 <fernandes.r1999@gmail.com>
2022-08-07 10:54:32 -0300

    Update Favicon

--- src/components/fields/index.tsx
+++ src/components/fields/index.tsx
@@ -9,25 +9,25 @@ import field_beet_mature from "../../assets/img/field_beet_mature.svg";
 import field_beet_growing from "../../assets/img/field_beet_growing.svg";
 
 export const fields = {
-    empty: () => <img src={field_empty} />,
+    empty: () => <img src={field_empty} alt="Field empty" />,
 
     tomato: {
-        growing: () => <img src={field_tomato_growing} />,
-        mature: () => <img src={field_tomato_mature} />
+        growing: () => <img src={field_tomato_growing} alt="Tomato growing" />,
+        mature: () => <img src={field_tomato_mature} alt="Tomato mature" />
     },
 
     bellPepper: {
-        growing: () => <img src={field_bellpepper_growing} />,
-        mature: () => <img src={field_bellpepper_mature} />,
+        growing: () => <img src={field_bellpepper_growing} alt="Bell Pepper growing" />,
+        mature: () => <img src={field_bellpepper_mature} alt="Bell Pepper growing" />,
     },
 
     carrot: {
-        growing: () => <img src={field_carrot_growing} />,
-        mature: () => <img src={field_carrot_mature} />,
+        growing: () => <img src={field_carrot_growing} alt="Carrot growing" />,
+        mature: () => <img src={field_carrot_mature} alt="Carrot growing" />,
     },
 
     beet: {
-        growing: () => <img src={field_beet_growing} />,
-        mature: () => <img src={field_beet_mature} />,
+        growing: () => <img src={field_beet_growing} alt="Beet growing" />,
+        mature: () => <img src={field_beet_mature} alt="Beet growing" />,
     }
 }
